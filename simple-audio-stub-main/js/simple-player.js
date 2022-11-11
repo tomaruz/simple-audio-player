c8bee7c282ce186b6158cc9a92c28fd171d5ea51
tomaruz <tomaruz2002@gmail.com>
2022-11-11 19:21:05 +0000

    updated

--- simple-audio-stub-main/js/simple-player.js
+++ simple-audio-stub-main/js/simple-player.js
@@ -5,7 +5,7 @@ const playPauseButton = document.getElementById('play-pause-button');
 const audioOnButton = document.getElementById('audio-on-button');
 const scrollBar = document.getElementById('scroll-bar');
 const volumeSlider = document.getElementById('volume-slider');
-const audio = new Audio("audio/Soft-Background-for-Interview.webm");
+const audio = new Audio("audio/Lazy Walk - Cheel.mp3");
 const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
@@ -13,30 +13,33 @@ const image4 = document.getElementById("image4");
 const imagearray = [image1, image2, image3, image4];
 
 //when page reloaded image is already selected with it is track
-window.addEventListener("load", () =>{imageonclick(image1);
-}, playPauseButton.src = "images/play.svg");
+window.addEventListener("load", () => {
+    imageonclick(image1);
+});
 
 let isPlaying = false;
-
 let isScrolling = false;
 //BUTTON LISTENER
 
 playPauseButton.onclick = function () {
-    // console.log("on click is playing = " + isPlaying) 
     if (audio.paused) {
         audio.play();
-        // isPlaying = true;
-    } 
+    }
     else {
         audio.pause();
-        // isPlaying = false;
     }
-    //AUDIO LISTENERS
-    //AUDIO EVENT LISTENERS
-    // event triggered once audio loaded
+
 }
 
+// audioOnButton.addEventListener = function () {
+//     if (volumeSlider.range.value > 1) {
+//         audioOnButton.src = "images/no-volume.png";
+// }
+// }
 
+//AUDIO LISTENERS
+//AUDIO EVENT LISTENERS
+// event triggered once audio loaded
 audio.oncanplaythrough = function () {
     scrollBar.disabled = false;
 }
@@ -45,7 +48,7 @@ function imageonclick(image) {
     for (let i = 0; i < imagearray.length; i++) {
         if (imagearray[i] == image) {
             imagearray[i].classList.add("activeimage")
-           
+
         }
         else {
             imagearray[i].classList.remove("activeimage")
@@ -55,32 +58,28 @@ function imageonclick(image) {
 
 image1.onclick = function () {
     // console.log("is playing = " + isPlaying) 
-    audio.src = "audio/Soft-Background-for-Interview.webm"
-    playPauseButton.src = "images/pause.svg"
+    audio.src = "audio/Lazy Walk - Cheel.mp3"
     imageonclick(image1)
     audio.play()
 }
 
 image2.onclick = function () {
     // console.log("is playing = " + isPlaying) 
-    audio.src = "audio/Hey There - half.cool.mp3"
-    playPauseButton.src = "images/pause.svg"
+    audio.src = "audio/Classic Mariachi - Jimena Contreras.mp3"
     imageonclick(image2)
     audio.play()
 }
 
 image3.onclick = function () {
     // console.log("is playing = " + isPlaying) 
-    audio.src = "audio/It Was a Time - TrackTribe.mp3"
-    playPauseButton.src = "images/pause.svg"
+    audio.src = "audio/Walking in the Sky - Nico Staf.mp3"
     imageonclick(image3)
     audio.play()
 }
 
 image4.onclick = function () {
     // console.log("is playing = " + isPlaying) 
-    audio.src = "audio/Mulholland - King Canyon.mp3"
-    playPauseButton.src = "images/pause.svg"
+    audio.src = "audio/In Memory of Jean Talon - Mini Vandals.mp3"
     imageonclick(image4)
     audio.play()
 }
@@ -95,17 +94,14 @@ audio.onpause = function () {
     playPauseButton.src = "images/play.svg"
 }
 
-//event triggered by meta data load
+
+//event triggered by metadata load 
 audio.onloadedmetadata = function () {
-    // console.log("is playing = " + isPlaying) 
-    totalTime.innerHTML = formatTime(audio.duration);
-    currentTime.innerHTML = formatTime(0);
-    scrollBar.innerHTML = Math.floor(audio.duration);
-    if (isPlaying) {
-        audio.play()
-        // isPlaying = false
-    }
+    totalTime.innerHTML = formatTime(audio.duration)
+    currentTime.innerHTML = formatTime(0)
+    scrollBar.max = Math.floor(audio.duration)
 }
+
 //event triggered when time updates
 audio.ontimeupdate = function () {
     currentTime.innerHTML = formatTime(audio.currentTime);
@@ -138,7 +134,23 @@ volumeSlider.addEventListener('input', (event) => {
     const value = event.target.value;
     // have to devide by 100 because it can only be from 0 to 1
     audio.volume = value / 100;
+
+    //when manually scrolling volume off and on, volume icon changes
+    if (audio.volume == 0) {
+        audioOnButton.src = "images/volume-off.png"
+    }
+    else {
+        audioOnButton.src = "images/speaker-filled-audio-tool.png"
+    }
 });
+
+//when click on the volume icon, turns the sound off
+audioOnButton.onclick = function () {
+    if (audioOnButton.src = "images/volume-off.png") {
+        volumeSlider.value = 0;
+        audio.volume = 0;
+    }
+}
 
 
 //UTILITY FUNCTIONS
